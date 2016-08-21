d21bcdfa35d0734dd60007d90744cd2568579def
dcrelling <david_crelling@hotmail.com>
2016-08-21 19:05:47 -0400

    first commit

---
+++ app/src/main/java/com/example/dcrelling/guardian/adapters/ArticleAdapter.java
@@ -0,0 +1,41 @@
+package com.example.dcrelling.guardian.adapters;
+
+import java.util.List;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import com.example.dcrelling.guardian.R;
+import com.example.dcrelling.guardian.services.ArticleSearchResponse;
+
+/**
+ * Created by dcrelling on 8/21/16.
+ */
+
+public class ArticleAdapter extends ArrayAdapter<ArticleSearchResponse.Article>
+{
+  public ArticleAdapter(Context context, List<ArticleSearchResponse.Article> objects)
+  {
+    super(context, 0, objects);
+  }
+
+
+  @NonNull
+  @Override
+  public View getView(int position, View convertView, ViewGroup parent)
+  {
+    ArticleSearchResponse.Article article = getItem(position);
+    if (convertView == null)
+    {
+      convertView = LayoutInflater.from(getContext()).inflate(R.layout.article_list_item, parent, false);
+    }
+    TextView webTitle = (TextView) convertView.findViewById(R.id.web_title);
+    webTitle.setText(article.webTitle);
+    return convertView;
+
+  }
+}
---
+++ app/src/main/java/com/example/dcrelling/guardian/services/ArticleSearchResponse.java
@@ -0,0 +1,57 @@
+package com.example.dcrelling.guardian.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by dcrelling on 8/21/16.
+ */
+
+public class ArticleSearchResponse
+{
+  public Response response;
+
+
+  public Response getResponse()
+  {
+    return this.response;
+  }
+
+
+  public class Response
+  {
+
+    @SerializedName("results")
+    List<Article> _articleList;
+
+
+    public Response()
+    {
+      _articleList = new ArrayList<>();
+    }
+
+
+    public List<Article> getArticleList()
+    {
+      return _articleList;
+    }
+
+  }
+
+
+  public class Article
+  {
+    public String id;
+    public String type;
+    public String sectionId;
+    public String section;
+    public String webPublicationDate;
+    public String webTitle;
+    public String webUrl;
+    public String apiUrl;
+  }
+
+
+}
