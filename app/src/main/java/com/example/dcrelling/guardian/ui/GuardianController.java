8409972cdca2f2ba67c47715f84f2e1b2669b8e1
dcrelling <david_crelling@hotmail.com>
2016-08-30 21:54:21 -0400

    some refactoring, setting a logo and updating a view

--- app/src/main/java/com/example/dcrelling/guardian/ui/GuardianController.java
+++ app/src/main/java/com/example/dcrelling/guardian/ui/GuardianController.java
@@ -3,9 +3,7 @@ package com.example.dcrelling.guardian.ui;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -41,17 +39,6 @@ public class GuardianController extends AppCompatActivity
     setContentView(R.layout.activity_main);
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
-
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-    fab.setOnClickListener(new View.OnClickListener()
-    {
-      @Override
-      public void onClick(View view)
-      {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAction("Action", null).show();
-      }
-    });
 
     DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
     ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
